012f3f5161d470643bb5f6f21b21ea2683888221
riashah345 <riashah345@gmail.com>
2021-09-23 09:48:48 -0400

    9/23 9:48AM update

--- src/albums/Album.java
+++ src/albums/Album.java
@@ -21,10 +21,48 @@ public class Album {
         this.releaseDate = null;
     }
 
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public Genre getGenre() {
+        return genre;
+    }
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
     //...
     @Override
-    public boolean equals(Object obj) {return true;}
+    public boolean equals(Object obj) {
+        Album newAlbum = (Album)obj;
+        if (newAlbum.title == this.title && newAlbum.artist == this.artist)
+            return true;
+        else
+            return false;
+    }
 //...
     @Override
-    public String toString() {return "";}
+    public String toString() {
+        String avail;
+        if (isAvailable)
+            avail = "is available";
+        else
+            avail = "is not available";
+        return title + "::" + artist + "::" + genre.name() + "::" + releaseDate.toString() + avail;
+    }
 }
